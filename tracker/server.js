5d5b40a933d01d43cdafedc82a054d7e0519c459
Maxim Bushmakov <Maksim_Bushm@mail.ru>
2020-01-17 08:00:21 +0300

    Some changes

--- tracker/server.js
+++ tracker/server.js
@@ -1,5 +1,5 @@
 // import { createServer } from "http";
-http_module = require("http");
+let http_module = require("http");
 let port = 8080;
 
 function parseCookies(request) {
@@ -15,20 +15,22 @@ function parseCookies(request) {
 
 function getRequestParams(request) {
 
-    reqParamsArr = request.url.split('&');
+    let reqParamsArr = request.url.split('&');
     reqParamsArr[0] = reqParamsArr[0].split('?')[1];
 
-    reqParamsDict = {};
-    reqLen = reqParamsArr.length;
+    let reqParamsDict = {};
+    const reqLen = reqParamsArr.length;
     for (let i = 0; i < reqLen; ++i) {
-        reqParamParts = reqParamsArr[i].split('=');
+        let reqParamParts = reqParamsArr[i].split('=');
         reqParamsDict[reqParamParts[0]] = reqParamParts[1];
     }
-    
+
     return reqParamsDict;
 }
 
-function HandleRequest(request, response) {
+
+// Tracking and responding of image
+function trackByImg(request, response) {
 
     response.writeHead(200, {
         "Content-type": "text/html",
@@ -42,15 +44,47 @@ function HandleRequest(request, response) {
         console.log("id: " + victimId);
     } else console.log("id: " + cookies["id"]);
 
-    requestParams = getRequestParams(request);
+    const requestParams = getRequestParams(request);
     console.log("url: " + requestParams["url"]);
 
     const trackImg = new Buffer.from('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7', 'base64');
     response.end(trackImg);
+
+    // let reader = new FileReader();
+    // reader.onload = () => {response.end(reader.result);}
+    // reader.readAsArrayBuffer("c:/users/user/desktop/vscode/prog/tracker/icon.jpg");
 }
 
 
-// server = createServer(HandleRequest);
-server = http_module.createServer(HandleRequest);
+function respondeJavascript(response) {
+    
+    response.writeHead(200, {
+        "Content-type": "text/javascript",
+        "charset": "utf-8",
+    });
+
+    let script = `
+    console.log('script loaded');
+    `;
+    response.end(script);
+}
+
+
+function Router(request, response) {
+
+    const reqPath = request.url.split('?')[0].slice(1);
+    console.log(reqPath);
+
+    if (reqPath == "img") {
+        trackByImg(request, response);
+
+    } else if (reqPath == "script") {
+        respondeJavascript(response);
+
+    } else {response.writeHead(404, {});}
+}
+
+
+server = http_module.createServer(Router);
 victimId = 0;
 server.listen(port, () => {console.log("Server is working.")});
